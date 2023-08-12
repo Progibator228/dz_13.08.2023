08c42d4cd613d948e05d1c4ae5d21e616c3da2a8
«Mouse» <Taxon2494@mail.ru>
2023-08-13 03:21:07 +0500

    Сделал дз

---
+++ task_10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+int num = new Random().Next(100,1000);
+System.Console.WriteLine($"Было сгенерированно число {num}");
+int digit1 = num / 100;
+int digit2 = num % 100;
+int digit3 = digit2 / 10;
+int digit4 = digit2 % 10;
+System.Console.WriteLine($"Вторая цифра в этом трехзначном числе {digit3}");
---
+++ task_13/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите  число от 100 до 1.000.000");
+int num1 = Convert.ToInt32 ( Console.ReadLine () );
+if ( num1 > 99 && num1 < 1000 )
+    {
+        int digit1 = num1 / 100;
+        int digit2 = num1 % 100;
+        int digit3 = digit2 / 10;
+        int digit4 = digit2 % 10;
+        System.Console.WriteLine($" Третья цифра слева это {digit4}");
+    }
+else if ( num1 > 1000 && num1 < 10000 )
+    {
+        int digit4 = num1 % 1000;
+        int digit1 = digit4 % 100;
+        int digit2 = digit1 / 10;
+        System.Console.WriteLine($" Третья цифра слева это {digit2}");
+    }
+else if ( num1 > 10000 && num1 < 100000 )
+    {
+        int digit4 = num1 % 1000;
+        int digit1 = digit4 / 100;
+        System.Console.WriteLine($" Третья цифра слева это {digit1}");
+    }
+else if ( num1 > 100000 && num1 < 1000000 )
+    {
+        int digit1 = num1 / 1000;
+        int digit4 = digit1 % 10;
+        System.Console.WriteLine($" Третья цифра слева это {digit4}");
+    }
+else
+    {
+        System.Console.WriteLine("Введенное число меньше 100");
+    }
